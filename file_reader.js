aeb3830c5cfba3d726ae1997eaeb8c9d74c0e3f3
KennyAle <kennyviver@gmail.com>
2024-11-19 12:21:22 -0800

    Callbacks

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
+  const name = data1
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    const lastName = data2
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      const age = data3
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const hobbies = JSON.parse(data4)
+        console.log(`${name} ${lastName} is ${age} years old and his hoobies are ${hobbies[0]} and ${hobbies[1]}`)
+      })
+    })
+  })
   // your code here
 })
